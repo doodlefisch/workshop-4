f7aaccac879e9af47d52df4676b191e0c3c1021d
doodlefisch <romain.giraud0212@gmail.com>
2024-03-26 17:34:03 +0100

    1.1 spin up nodes

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -9,6 +9,34 @@ export async function simpleOnionRouter(nodeId: number) {
 
   // TODO implement the status route
   // onionRouter.get("/status", (req, res) => {});
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: string | null = null;
+  
+  onionRouter.post("/relayMessage", async (req, res) => {
+    const { message, nextNodeId } = req.body;
+    lastReceivedEncryptedMessage = message;
+    lastMessageDestination = nextNodeId;
+    
+  
+    res.send("Message relayed successfully");
+  });
+  
+  onionRouter.get("/status", (req, res) => {
+    res.send("live");
+  });
+  onionRouter.get('/getLastReceivedEncryptedMessage' , (req, res) => {
+    res.send({"result":lastReceivedEncryptedMessage});
+  });
+
+
+  onionRouter.get('/getLastReceivedDecryptedMessage' , (req, res) => {
+    res.send({"result":lastReceivedDecryptedMessage});
+  });
+
+  onionRouter.get('/getLastMessageDestination' , (req, res) => {
+    res.send({"result":lastMessageDestination});
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
